8668723e88fbd7254291426161f9cee219b219ad
Dan Brakeley <dan@brakeley.net>
2021-10-12 13:07:26 -0400

    fix more build info

--- cmd/actiontest/main.go
+++ cmd/actiontest/main.go
@@ -10,7 +10,6 @@ func main() {
 	fmt.Printf("NEW ACTION TEST PRIME MARK 2 _FINAL\n")
 	fmt.Printf("Version: %s\n", buildvar.Version)
 	fmt.Printf("Build Time: %s\n", buildvar.BuildTime)
-	fmt.Printf("Release Name: %s\n", buildvar.RelaseName)
-	fmt.Printf("Release Description: %s\n", buildvar.ReleaseDescription)
+	fmt.Printf("Release Notes: %s\n", buildvar.ReleaseNotes)
 	fmt.Printf("Release URL: %s\n", buildvar.ReleaseURL)
 }
--- internal/buildvar/buildvar.go
+++ internal/buildvar/buildvar.go
@@ -3,14 +3,11 @@ package buildvar
 // -ldflags '-X "github.com/danbrakeley/actiontest/internal/buildvar.Version=${{ github.event.release.tag_name }}"'
 var Version string
 
-// -ldflags '-X "github.com/danbrakeley/actiontest/internal/buildvar.BuildTime=${{ github.event.release.tag_name }}"'
+// -ldflags '-X "github.com/danbrakeley/actiontest/internal/buildvar.BuildTime=${{ github.event.release.created_at }}"'
 var BuildTime string
 
-// -ldflags '-X "github.com/danbrakeley/actiontest/internal/buildvar.ReleaseName=${{ github.event.release.name }}"'
-var RelaseName string
-
 // -ldflags '-X "github.com/danbrakeley/actiontest/internal/buildvar.ReleaseDescription=${{ github.event.release.body }}"'
-var ReleaseDescription string
+var ReleaseNotes string
 
 // -ldflags '-X "github.com/danbrakeley/actiontest/internal/buildvar.ReleaseURL=${{ github.event.release.html_url }}"'
 var ReleaseURL string
